d921d93f55b1fd2ced447d9fee4d8fb2996d71d2
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-13 16:23:40 +0200

    v

--- application/models/InsererUnProduit.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-public function insererUnProduit($pDonneesAInserer)
-
-{
-
-    return $this->db->insert('noProduit', $pDonneesAInserer);
-
-} // insererUnProduit
--- application/views/visiteur/connexionReussie.php
+++
@@ -1,9 +0,0 @@
-<h2>Connexion réussie !</h2>
-
-<?php echo '<p>Bienvenue '.$Identifiant.' !</p>';?>
-
- 
-
-<p><a href="<?php echo site_url('visiteur/listerLesproduits') ?>">Retour à la liste des produits</a><p>
-
-<!-- ou echo anchor('visiteur/listerTousLesProduits','Retour à la liste des produits'); -->
