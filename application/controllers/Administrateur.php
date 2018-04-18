dd0d193133b8f38917dd106a3918ccdcd017b3fd
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-18 10:36:53 +0200

    v

--- application/controllers/Administrateur.php
+++ application/controllers/Administrateur.php
@@ -1,52 +1,28 @@
 <?php
 class Administrateur extends CI_Controller {
     public function ajouterUnArticleHTML5()
-
     {
-    
       $this->load->helper('form');
-    
       $DonneesInjectees['TitreDeLaPage'] = 'Ajouter un article';
-    
-     
-    
+             
       if ($this->input->post('boutonAjouter')) // On test si le formulaire a été posté.
-    
       {
-    
-    // le bouton 'submit', boutonAjouter est <> de NULL, on a posté quelque chose.
-    
-    $donneesAInserer = array(
-    
-      'cTitre' => $this->input->post('txtTitre'),
-    
-      'cTexte' => $this->input->post('txtTexte'),
-    
-      'cNomFichierImage' => $this->input->post('txtNomFichierImage')
-    
-    ); // cTitre, cTexte, cNomFichierImage : champs de la table tabarticle
-    
-    $this->ModeleArticle->insererUnArticle($donneesAInserer); // appel du modèle
-    
-    $this->load->helper('url'); // helper chargé pour utilisation de site_url (dans la vue)
-    
-    $this->load->view('administrateur/insertionReussie');
-    
+        // le bouton 'submit', boutonAjouter est <> de NULL, on a posté quelque chose.
+        $donneesAInserer = array(
+          'cTitre' => $this->input->post('txtTitre'),
+          'cTexte' => $this->input->post('txtTexte'),
+          'cNomFichierImage' => $this->input->post('txtNomFichierImage')
+        ); // cTitre, cTexte, cNomFichierImage : champs de la table tabarticle
+        $this->ModeleArticle->insererUnArticle($donneesAInserer); // appel du modèle
+        $this->load->helper('url'); // helper chargé pour utilisation de site_url (dans la vue)
+        $this->load->view('administrateur/insertionReussie');
       }
-    
       else
-    
       {  
-    
-    /* si formulaire non posté = bouton 'submit' à NULL : on est jamais passé par le formulaire -> on envoie le formulaire */
-    
-    $this->load->view('templates/Entete');
-    
-    $this->load->view('administrateur/ajouterUnArticleHTML5', $DonneesInjectees);
-    
-    $this->load->view('templates/PiedDePage');
-    
+        /* si formulaire non posté = bouton 'submit' à NULL : on est jamais passé par le formulaire -> on envoie le formulaire */
+        $this->load->view('templates/Entete');
+        $this->load->view('administrateur/ajouterUnArticleHTML5', $DonneesInjectees);
+        $this->load->view('templates/PiedDePage');
       }
-    
     } // ajouterUnArticleHTML5
 }
--- application/models/ModeleArticle.php
+++ application/models/ModeleArticle.php
@@ -1,12 +1,7 @@
 <?php
 class ModeleArticle extends CI_Controller {
     public function insererUnArticle($pDonneesAInserer)
-
     {
-   
       return $this->db->insert('tabarticle', $pDonneesAInserer);
-   
     } // insererUnArticle
-   
-    
 }
