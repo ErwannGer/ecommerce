f5c2ff101d5b3f6ebb76c900f4e41eea2d6a4de4
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-18 09:58:57 +0200

    v

--- application/models/ModeleArticle.php
+++ application/models/ModeleArticle.php
@@ -1,7 +1,12 @@
 <?php
 class ModeleArticle extends CI_Controller {
-  public function insererUnArticle($pDonneesAInserer)
-        {
-            return $this->db->insert('tabarticle', $pDonneesAInserer);
-        }
+    public function insererUnArticle($pDonneesAInserer)
+
+    {
+   
+      return $this->db->insert('tabarticle', $pDonneesAInserer);
+   
+    } // insererUnArticle
+   
+    
 }
--- application/views/administrateur/ajouterUnArticleHTML5.php
+++ application/views/administrateur/ajouterUnArticleHTML5.php
@@ -1,19 +1,37 @@
 <h2><?php echo $TitreDeLaPage ?></h2>
 
+ 
+
 <?php
+
+ 
 
 echo form_open('administrateur/ajouterUnArticleHTML5');
 
+ 
+
 echo form_label("Titre de l'article : ", 'lbltTitre');
-echo form_input('txtTitre', '', array('pattern' =>'[a-zA-Z]*','required'=>'required', 'title'=>'Saisir des lettres uniquement')).'<BR>';
+
+echo form_input('txtTitre','',array('pattern' =>'[a-zA-Z]*','required'=>'required', 'title'=>'Saisir des lettres uniquement')).'<BR>';
+
+ 
 
 echo form_label("Texte de l'article : ", 'lblTexte');
-echo form_textarea('txtTexte', '', array('required'=>'required')).'<BR>';
 
-echo form_label('Nom du fichier Image : ', 'lblNomFichierImage');
-echo form_input('txtNomFichierImage', '', array('pattern'=>'^[a-zA-Z][a-zA-Z0-9]*','title'=>'Un nom de fichier doit commencer par une lettre', 'required'=>'required')).'<BR>';
+echo form_textarea('txtTexte', '',array('required'=>'required')).'<BR>';
+
+ 
+
+echo form_label('Nom du fichier Image : ','lblNomFichierImage');
+
+echo form_input('txtNomFichierImage', '',array('pattern'=>'^[a-zA-Z][a-zA-Z0-9]*','title'=>'Un nom de fichier doit commencer par une lettre', 'required'=>'required')).'<BR>';
+
+ 
 
 echo form_submit('boutonAjouter', 'Ajouter un article').'<BR>';
+
 echo form_close();
+
+ 
 
 ?>
