9f5eb1a2b5bb843e60c154ddf6a7b3f9d10b2ce0
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-18 10:55:59 +0200

    v

--- application/controllers/Visiteur.php
+++ application/controllers/Visiteur.php
@@ -2,3 +2,25 @@
 //class Visiteur extends CI_Controller {
    // public function index()
        // $this->load->view('templates/index');
+       class Visiteur extends CI_Controller {
+ 
+        public function __construct()   
+        {
+            parent::__construct();
+            $this->load->helper('url');
+            $this->load->helper('assets');
+            $this->load->library("pagination");
+            $this->load->model('ModeleArticle');
+            $this->load->model('ModeleUtilisateur');
+        }
+           
+        public function listerLesArticles()
+        {
+            $DonneesInjectees['lesArticles'] = $this->ModeleArticle->retournerArticles();
+            $DonneesInjectees['TitreDeLaPage'] = 'Tous les articles';
+           
+            $this->load->view('templates/Entete');
+            $this->load->view('visiteur/listerLesArticles', $DonneesInjectees);
+            $this->load->view('templates/PiedDePage');
+        }
+       }
