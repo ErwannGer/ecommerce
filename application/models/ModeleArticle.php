4dcbd893d7684d95b6e1e9dbb0904c01e0cef126
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-18 15:54:07 +0200

    v

--- application/models/ModeleArticle.php
+++ application/models/ModeleArticle.php
@@ -2,6 +2,6 @@
 class ModeleArticle extends CI_Controller {
     public function insererUnArticle($pDonneesAInserer)
     {
-      return $this->db->insert('tabarticle', $pDonneesAInserer);
-    } // insererUnArticle
+      return $this->db->insert('produit', $pDonneesAInserer);
+    }
 }
