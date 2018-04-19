4d59a0f4c01db9ca9e1114f86af49a96101e8c8e
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-19 11:53:34 +0200

    v

--- application/controllers/Administrateur.php
+++ application/controllers/Administrateur.php
@@ -1,5 +1,19 @@
 <?php
 class Administrateur extends CI_Controller {
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('ModeleArticle');
+    $this->load->library('session');
+    if ($this->session->statut==0)
+    {
+      $this->load->helper('url');
+      redirect('/visiteur/seConnecter');
+    }
+
+  }
+
     public function ajouterUnArticleHTML5()
     {
       $this->load->helper('form');
--- application/controllers/Visiteur.php
+++ application/controllers/Visiteur.php
@@ -1,7 +1,4 @@
-//<?php
-//class Visiteur extends CI_Controller {
-   // public function index()
-       // $this->load->view('templates/index');
+<?php
        class Visiteur extends CI_Controller {
  
         public function __construct()   
@@ -22,5 +19,51 @@
             $this->load->view('templates/Entete');
             $this->load->view('visiteur/listerLesArticles', $DonneesInjectees);
             $this->load->view('templates/PiedDePage');
+        }
+    
+        public function seConnecter()
+        {
+          $this->load->helper('form');
+          $this->load->library('form_validation');
+       
+          $DonneesInjectees['TitreDeLaPage'] = 'Se connecter';
+       
+          $this->form_validation->set_rules('txtIdentifiant', 'Identifiant', 'required');
+          $this->form_validation->set_rules('txtMotDePasse', 'Mot de passe', 'required');
+       
+          if ($this->form_validation->run() === FALSE)
+          {
+
+            $this->load->view('templates/Entete');  
+            $this->load->view('visiteur/seConnecter', $DonneesInjectees); // on renvoie le formulaire  
+            $this->load->view('templates/PiedDePage');
+          }
+          else
+          {
+            $Utilisateur = array(
+                'cIdentifiant' => $this->input->post('txtIdentifiant'),
+                'cMotDePasse' => $this->input->post('txtMotDePasse'),
+            );
+       
+            $UtilisateurRetourne = $this->ModeleUtilisateur->retournerUtilisateur($Utilisateur);
+            if (!($UtilisateurRetourne == null))
+            { 
+                $this->load->library('session');
+                $this->session->identifiant = $UtilisateurRetourne->cIdentifiant;
+                $this->session->statut = $UtilisateurRetourne->cStatut;
+       
+                $DonneesInjectees['Identifiant'] = $Utilisateur['cIdentifiant'];
+                $this->load->view('templates/Entete');
+                $this->load->view('visiteur/connexionReussie', $DonneesInjectees);
+                $this->load->view('templates/PiedDePage');
+            }
+            else
+            {
+                $this->load->view('templates/Entete');
+                $this->load->view('visiteur/seConnecter', $DonneesInjectees);
+                $this->load->view('templates/PiedDePage');
+            }  
+       
+           }
         }
        }
