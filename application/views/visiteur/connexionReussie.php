55ac91ef52bdbb604ae7467262652d785082f487
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-23 15:31:35 +0200

    v

--- application/views/visiteur/connexionReussie.php
+++ application/views/visiteur/connexionReussie.php
@@ -1,4 +1,3 @@
 <h2>Connexion réussie !</h2>
-<?php echo '<p>Bienvenue '.$Identifiant.' !</p>';
-?>
+<?php echo '<p>Bienvenue '.$Identifiant.' !</p>';?>
 <p><a href="<?php echo site_url('Visiteur/ListerLesArticles') ?>">Voir la liste des articles</a><p>
--- application/views/visiteur/seConnecter.php
+++ application/views/visiteur/seConnecter.php
@@ -17,3 +17,29 @@ echo form_close();
 <h2>Créer un compte</h2>
 
 <p><a href="<?php echo site_url('Visiteur/creerUnCompte') ?>"</a><p>
+
+<?php
+ 
+echo form_open('visiteur/creerUnCompte');
+  
+echo form_label("Nom : ", 'lblNom');
+echo form_input('txtNom','', array('pattern' => '[a-zA-Z]*', 'required'=>'required', 'title' => 'Saisir uniquement des lettres')).'<BR>';
+
+echo form_label("Prénom : ", 'lblPrenom');
+echo form_input('txtPrenom','', array('pattern' => '[a-zA-Z]*', 'required'=>'required', 'title' => 'Saisir uniquement des lettres')).'<BR>';
+ 
+echo form_label("Adresse : ", 'lblAdresse');
+echo form_input('txtAdresse','', array('pattern' => '[a-zA-Z0-9]*', 'required'=>'required')).'<BR>';
+
+echo form_label("Ville : ", 'lblVille');
+echo form_input('txtVille','', array('pattern' => '[a-zA-Z]*', 'required'=>'required', 'title' => 'Saisir uniquement des lettres')).'<BR>';
+ 
+echo form_label("Code postal : ", 'lblCodePostal');
+echo form_input('txtCodePostal','', array('pattern' => '[0-9]*', 'required'=>'required', 'title' => 'Saisir uniquement des nombres')).'<BR>';
+ 
+echo form_label("Email : ", 'lblEmail');
+echo form_input('txtEmail','', array('pattern' => '[a-zA-Z0-9]*', 'required'=>'required')).'<BR>';
+
+echo form_label("Mot de passe : ", 'lblMotDePasse');
+echo form_input('txtMotDePasse','', array('pattern' => '[a-zA-Z0-9]*', 'required'=>'required', 'title' => 'Saisir uniquement des lettres')).'<BR>';
+ 
