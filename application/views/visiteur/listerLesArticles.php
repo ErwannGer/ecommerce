4297fa966bde924f55781eafed1d075ca565313f
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-23 14:59:11 +0200

    v

--- application/views/visiteur/listerLesArticles.php
+++ application/views/visiteur/listerLesArticles.php
@@ -1,5 +1,5 @@
 <h2><?php echo $TitreDeLaPage ?></h2>
-
 <?php foreach ($lesArticles as $unArticle):
      echo '<h3>'.anchor('visiteur/voirUnArticle/'. $unArticle['NOPRODUIT'], $unArticle['LIBELLE']).'</h3>';
-endforeach ?>
+endforeach?>
+
