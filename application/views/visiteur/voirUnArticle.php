5a32f5cd029df6a847f95a9571361b6d10460561
ErwannGer <ewrann.gernigon@outlook.fr>
2018-04-23 14:29:49 +0200

    V

--- application/views/visiteur/voirUnArticle.php
+++ application/views/visiteur/voirUnArticle.php
@@ -2,4 +2,8 @@
 echo '<h2>'.$unArticle['LIBELLE'].'</h2>';
 echo $unArticle['DETAIL'];
 echo '<p>'.img($unArticle['NOMIMAGE']).'<p>';
+echo $unArticle['PRIXHT'].'<p>';
+echo $unArticle['TAUXTVA'].'<p>';
+echo $unArticle['QUANTITEENSTOCK'].'<p>';
+echo $unArticle['DISPONIBLE'];
 echo '<p>'.anchor('visiteur/listerLesArticles','Retour à la liste des articles').'</p>';
